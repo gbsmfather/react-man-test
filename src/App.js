171398c4611f2b747aa7496f78ab8208bebe4509
Daphnis <iyahn@keti.re.kr>
2021-05-05 20:59:40 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        hello: 'hello app js!',
+        count: 0,
+    };
+
+    handleChange = () => {
+        this.setState({
+            hello: 'bye app js!',
+        });
+    }
+
+    countUp = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    };
+
+    render() {
+        return (
+            <div className="gray-background">
+                <img src={logo} lat="logo" alt="logo"/>
+                <h2>Let's develop management system!</h2>
+            </div>
+        );
+    }
 }
+//
+// class InsideApp extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.count}
+//                 <button onClick={this.props.handleChange}>click Me ! 2</button>
+//             </div>
+//         )
+//     };
+// }
 
 export default App;
