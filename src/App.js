23185a59d66798a378a3c35fd25d9833a37b9f60
Daphnis <iyahn@keti.re.kr>
2021-05-05 22:37:00 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import {Component} from "react";
 import Customer from "./components/Customer";
 
+import {Paper} from "@material-ui/core";
+import {Table} from "@material-ui/core";
+import {TableHead} from "@material-ui/core";
+import {TableBody} from "@material-ui/core";
+import {TableRow} from "@material-ui/core";
+import {TableCell} from "@material-ui/core";
+
+import {withStyles} from "@material-ui/core";
+
+const styles = (theme) => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+        overflowX: 'auto'
+    },
+    table: {
+        minWidth: 1080
+    }
+});
+
 const customers = [{
         'id': 1,
         'image': 'https://placeimg.com/64/64/1',
@@ -48,24 +68,25 @@ class App extends Component {
     };
 
     render() {
+        const {classes} = this.props;
         return (
-            <div>
-                {
-                    customers.map((c) => {
-                        return (
-                            <Customer
-                                key={c.id}
-                                id={c.id}
-                                image={c.image}
-                                name={c.name}
-                                birthday={c.birthday}
-                                gender={c.gender}
-                                job={c.job}
-                            />
-                        );
-                    })
-                }
-            </div>
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {customers.map((c) => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+                    </TableBody>
+                </Table>
+            </Paper>
         );
     }
 }
@@ -81,4 +102,4 @@ class App extends Component {
 //     };
 // }
 
-export default App;
+export default withStyles(styles)(App);
